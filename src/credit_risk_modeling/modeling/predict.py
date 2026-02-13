f9bf7a1ea0da87aa585cb16bffc78a166e0cf484
Billy Brothers <billy.brothers0@gmail.com>
2026-02-13 11:26:41 -0600

    edited to include risk reporting

--- src/credit_risk_modeling/modeling/predict.py
+++ src/credit_risk_modeling/modeling/predict.py
@@ -28,7 +28,7 @@ def batch_predict(
     """
     Score multiple applicants from CSV file.
     
-    Input CSV must have 12 columns (raw features):
+    Input CSV must have 11 columns (raw features):
     person_age, person_income, person_home_ownership, person_emp_length,
     loan_intent, loan_grade, loan_amnt, loan_int_rate, loan_percent_income,
     cb_person_default_on_file, cb_person_cred_hist_length
